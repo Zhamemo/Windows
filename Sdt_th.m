287a8174687d3c5abc6b6c3a26e3e5369bf35c5c
ZMM <15115238791@163.com>
2021-05-11 16:58:18 +0800

    revise Sdt_th.m

--- Sdt_th.m
+++ Sdt_th.m
@@ -1,22 +1,10 @@
-%斯密特正交单位化
-function b = Sdt_th(a)
-[m,n] = size(a);
-if(m < n)
-    error('行小于列，无法计算，请转置后重新输入');
-    return
-end
-b = zeros(m,n);
-%正交化
-b(:,1) = a(:,1);
-for i=2:n
-    for j = 1:i-1
-        b(:,i) = b(:,i)-dot(a(:,i),b(:,j)) / dot(b(:,j),b(:,j))*b(:,j);
-    end
-    b(:,i) = b(:,i) + a(:,i);
+%单位化
+function y = Sdt_th(x)
+
+num = size(x,1);
+y = zeros(num,1);
+for i=1:num
+    y(i,:) = x(i) / norm(x);
 end
 
-%单位化
-for k=1:n
-    b(:,k) = b(:,k) / norm(b(:,k));
-end
 end
