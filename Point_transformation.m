992a107e74aa212494cbb0e900048ef2daee5a94
ZMM <15115238791@163.com>
2021-05-22 16:51:14 +0800

    updata_pointtr

--- Point_transformation.m
+++ Point_transformation.m
@@ -1,7 +1,5 @@
 clear all;clc;
-load('ams_2.txt');
-load('ams_3.txt');
-load('ams_4.txt')
+load('ams1-2.txt');
 
     [filename, pathname] = uigetfile('*.txt', '导入观测数据');
     if isequal(filename,0)||isequal(pathname,0)
@@ -16,7 +14,7 @@ load('ams_4.txt')
         return;
     end
 
-Arsize = 31;
+Arsize = 52;
 hc = ones(Arsize,4);
 hc(:,1:3) = points;
 point = ones(Arsize,4);
@@ -25,7 +23,7 @@ for i=1:Arsize
 %     point(i,:) = ams_1 * ams_2 * hc(i,:)';
 %     point(i,:) = ams_1 * hc(i,:)';
 %     point(i,:) = ams_4 * ams_3 * ams_2 * hc(i,:)';
-    point(i,:) =  ams_2 * hc(i,:)';
+    point(i,:) =  ams1_2 \ hc(i,:)';
     
 end
 
@@ -34,7 +32,7 @@ data = zeros(Arsize,3);
 for k =1:Arsize
     data = point(:,1:3);
 end
-% dlmwrite('myFile.txt',M,'delimiter','\t','precision',3)
-p = fopen('2-3_1.txt');
-dlmwrite('2-3_1.txt',data,'delimiter','\t','precision','%.4f');
+n = 8;
+str = ['C2_1_',num2str(n),'.txt'];
+dlmwrite(str,data,'delimiter',',','precision','%.6f');
 
