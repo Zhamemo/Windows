b8b841ff0536b582a2da7232deb25f853869b44f
ZMM <15115238791@163.com>
2021-05-23 19:49:25 +0800

    revise_r_t

--- R_T.m
+++ R_T.m
@@ -1,11 +1,38 @@
 clear all;clc;
-load('ep.mat');
+load('ams.mat');
 
-Arsize =1;
+Arsize_1 = 28;
+
+%计算标定板间的外参
 RT = zeros(4,4);
-for i = 1:Arsize
-    RT_1(:,:,i) = Ep(:,:,i,2) / Ep(:,:,i,1);
-    RT = RT_1(:,:,i) + RT;
+for i = 1:Arsize_1
+    RT_1(:,:,i) = ams(:,:,i+28) \ ams(:,:,i);
 end
-% RT = RT / Arsize;
-% dlmwrite('RT.txt',RT,'delimiter','\t','precision','%.6f');
+
+%优化旋矩阵，采用dcm2angle,angle2dcm函数
+ro(:,:,:) = RT_1(1:3,1:3,:);
+[pitch, roll, yaw] = dcm2angle(ro);
+num_1 = size(pitch,1);
+pitch_1 = sum(pitch) / num_1;
+roll_1 = sum(roll) / num_1;
+yaw_1 = sum(yaw) / num_1;
+Ro = angle2dcm(pitch_1,roll_1,yaw_1);
+
+%优化平移向量
+Arsizez_2 = size(RT_1,3);
+tr = zeros(Arsizez_2,3);
+
+for j = 1:Arsizez_2
+    tr(j,:) = RT_1(1:3,4,j);
+end
+num_2 = size(tr,1);
+Tr(:,:) = sum(tr) / num_2;
+
+%拼装旋转矩阵和平移向量
+TransMatrix = zeros(4,4);
+TransMatrix(1:3,1:3) = Ro;
+TransMatrix(1:3,4) = Tr;
+TransMatrix(4,4) = 1; 
+
+dlmwrite('TransMatrix.txt',TransMatrix,'delimiter','\t','precision','%.6f');
+
